a4a4d99475e31e2974e1c78028f393a3ea9a0a0c
utti <jan.uthoff@rwth-aachen.de>
2018-11-13 13:18:08 +0100

    enable git functions

--- exercises/exercise_script.sh
+++ exercises/exercise_script.sh
@@ -33,11 +33,11 @@ inputtxt=""
 for i in `seq 1 $aufgno`; do
     inputtxt+=$(printf '\\input{%s}\n' "$(aufg_filename $i)")
     printf '\\begin{exercise}{0}\n\n\\end{exercise}\n' >> "$(aufg_filename $i)"
-#    git add "$(aufg_filename $i)"
+    git add "$(aufg_filename $i)"
 done
 
 printf "$template" "$ueb" "$inputtxt" > ueb$ueb.tex
-#git add ueb$ueb.tex
+git add ueb$ueb.tex
 
-#git commit -m "Skelett Uebung $ueb"
+git commit -m "Skelett Uebung $ueb"
 
